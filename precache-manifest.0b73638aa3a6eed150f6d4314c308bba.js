1773c7a02d6e6ab6f8a29d9e77495bebae78b974
Abner <sbner2@gmail.com>
2020-06-22 23:02:04 -0300

    Updates

---
+++ precache-manifest.0b73638aa3a6eed150f6d4314c308bba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ec52b04705c09e5cf74ea596f41f4a",
+    "url": "/facerecon/index.html"
+  },
+  {
+    "revision": "38eb8c9e3a34472fe990",
+    "url": "/facerecon/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1c2a5d7c312838686744",
+    "url": "/facerecon/static/css/main.9a96f44e.chunk.css"
+  },
+  {
+    "revision": "38eb8c9e3a34472fe990",
+    "url": "/facerecon/static/js/2.83739a87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecon/static/js/2.83739a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c2a5d7c312838686744",
+    "url": "/facerecon/static/js/main.3651c4aa.chunk.js"
+  },
+  {
+    "revision": "ab912bd8b1653dceeddd",
+    "url": "/facerecon/static/js/runtime-main.7ce5b355.js"
+  }
+]);
