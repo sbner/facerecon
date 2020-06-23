d011c54f6ea597b09429120c8b548ae2f5b070e2
Abner <sbner2@gmail.com>
2020-06-22 23:10:16 -0300

    Changed Placeholder and paragraph

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,11 +5,13 @@ const ImageLinkForm = ( {onInputChange,onButtonSubmit} ) => {
 	return (
 		<div>
 			<p className='f3'>
-			{'This magic app will detect faces in your pictures. Give it a try'}
+			{'This app will detect faces in your pictures. Give it a try'}
+			<br />
+			<em>{'Right now it only detects one face at a time.'}</em>
 			</p>
 			<div className='center'>
 				<div className='form center pa4 br3 shadow-5'>
-					<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+					<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} placeHolder='Paste an image URL here'/>
 					<button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
 					onClick={onButtonSubmit}
 					>Detect</button>
